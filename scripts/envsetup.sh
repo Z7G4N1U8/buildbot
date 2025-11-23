a5e0069862fccfacd21919619af136faeee1e33b
Peace <git@z7g4n1u8.dev>
2025-11-23 16:58:53 +0530

    feat: setup RBE

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -34,8 +34,61 @@ curl -LsSf https://astral.sh/uv/install.sh | bash
 git lfs install
 
 # Setup ccache
-cat << EOF >> ~/.bashrc
-USE_CCACHE=1
-CCACHE_EXEC=/usr/bin/ccache
-EOF
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
 ccache -M 50G
+
+# Setup RBE
+rm -rf $HOME/rbe client-linux-amd64.zip
+wget https://github.com/xyz-sundram/Releases/releases/download/client-linux-amd64/client-linux-amd64.zip
+unzip client-linux-amd64.zip -d $HOME/rbe
+
+export USE_RBE=1                                      
+export RBE_DIR="$HOME/rbe"                      # Path to the extracted reclient directory (relative or absolute)
+export NINJA_REMOTE_NUM_JOBS=128                        # Number of parallel remote jobs (adjust based on your RAM, buildbuddy has 80 CPU cores in the free tier)
+
+# --- BuildBuddy Connection Settings ---
+export RBE_service="aosp.buildbuddy.io:443"        # BuildBuddy instance address (without grpcs://, add the port 443)
+export RBE_use_rpc_credentials=false                   
+export RBE_service_no_auth=true                       
+
+# --- Unified Downloads/Uploads (Recommended) ---
+export RBE_use_unified_downloads=true
+export RBE_use_unified_uploads=true
+
+# --- Execution Strategies (remote_local_fallback is generally best) ---
+export RBE_R8_EXEC_STRATEGY=remote_local_fallback
+export RBE_D8_EXEC_STRATEGY=remote_local_fallback
+export RBE_JAVAC_EXEC_STRATEGY=remote_local_fallback
+export RBE_JAR_EXEC_STRATEGY=remote_local_fallback
+export RBE_ZIP_EXEC_STRATEGY=remote_local_fallback
+export RBE_TURBINE_EXEC_STRATEGY=remote_local_fallback
+export RBE_SIGNAPK_EXEC_STRATEGY=remote_local_fallback
+export RBE_CXX_EXEC_STRATEGY=remote_local_fallback    # Important see below.
+export RBE_CXX_LINKS_EXEC_STRATEGY=remote_local_fallback
+export RBE_ABI_LINKER_EXEC_STRATEGY=remote_local_fallback
+export RBE_ABI_DUMPER_EXEC_STRATEGY=    # Will make build slower, by a lot. Keeping this for documentation
+export RBE_CLANG_TIDY_EXEC_STRATEGY=remote_local_fallback
+export RBE_METALAVA_EXEC_STRATEGY=remote_local_fallback
+export RBE_LINT_EXEC_STRATEGY=remote_local_fallback
+
+# --- Enable RBE for Specific Tools ---
+export RBE_R8=1
+export RBE_D8=1
+export RBE_JAVAC=1
+export RBE_JAR=1
+export RBE_ZIP=1
+export RBE_TURBINE=1
+export RBE_SIGNAPK=1
+export RBE_CXX_LINKS=1
+export RBE_CXX=1
+export RBE_ABI_LINKER=1
+export RBE_ABI_DUMPER=    # Will make build slower, by a lot. Keeping this for documentation
+export RBE_CLANG_TIDY=1
+export RBE_METALAVA=1
+export RBE_LINT=1
+
+# --- Resource Pools ---
+export RBE_JAVA_POOL=default
+export RBE_METALAVA_POOL=default
+export RBE_LINT_POOL=default
