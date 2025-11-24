6a5d885b4ad522d4cdc46a340a5146071d8a0b81
Peace <git@z7g4n1u8.dev>
2025-11-24 13:26:47 +0530

    refactor(scripts/envsetup): more optimizations

--- scripts/gcpsetup.sh
+++ scripts/gcpsetup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# remount root as noatime
-sudo mount -o remount,rw,noatime /
+# Optimizing filesystem mount
+sudo mount -o remount,rw,noatime,barrier=0,commit=600,data=writeback /
 
 packages=(
   "7zip"
   "android-sdk-platform-tools" "aria2" "arj" "axel"
-  "bc" "bison" "brotli" "build-essential"
+  "bc" "bison" "brotli" "btop" "build-essential"
   "cabextract" "ccache" "cpio" "curl"
   "detox" "device-tree-compiler"
   "flex"
   "g++-multilib" "gawk" "gcc-multilib" "git" "git-lfs" "gnupg" "gperf"
   "imagemagick"
   "lib32ncurses-dev" "lib32readline-dev" "lib32z1-dev" "liblz4-dev" "liblz4-tool" "liblzma-dev" "libncurses6" "libncurses-dev" "libdw-dev" "libelf-dev" "libsdl1.2-dev" "libssl-dev" "libxml2" "libxml2-utils" "linux-modules-extra-$(uname -r)" "lz4" "lzop"
-  "mpack"
+  "micro" "mpack"
   "p7zip-full" "p7zip-rar" "pngcrush" "protobuf-compiler" "python3-pip" "python3-protobuf" "python-is-python3"
   "rar" "rename" "ripgrep" "repo" "rsync"
   "schedtool" "sharutils" "squashfs-tools"
@@ -30,6 +30,9 @@ sudo apt-get install -y "${packages[@]}"
 git config --global user.name "Peace"
 git config --global user.email "git@z7g4n1u8.dev"
 git config --global color.ui true
+git config --global core.preloadindex true
+git config --global core.untrackedCache true
+git config --global gc.auto 0
 
 # Install additional stuff
 curl -LsSf https://astral.sh/uv/install.sh | bash
@@ -53,11 +56,11 @@ sudo systemctl restart zramswap
 # Kernel Tuning (Memory + Network for RBE)
 sudo sysctl vm.swappiness=100 > /dev/null
 sudo sysctl vm.page-cluster=0 > /dev/null
-sudo sysctl vm.vfs_cache_pressure=50 > /dev/null
-sudo sysctl vm.dirty_background_bytes=419430400 > /dev/null
-sudo sysctl vm.dirty_bytes=1073741824 > /dev/null
+sudo sysctl vm.dirty_background_ratio=10 > /dev/null
+sudo sysctl vm.dirty_ratio=40 > /dev/null
 sudo sysctl -w net.core.default_qdisc=fq > /dev/null
 sudo sysctl -w net.ipv4.tcp_congestion_control=bbr > /dev/null
 
 # Setup ccache
 ccache -M 50G > /dev/null
+ccache -o compression=true
