1a71df30cd7f10362f994ae9779b196eee9197c8
Peace <git@z7g4n1u8.dev>
2025-11-29 17:14:56 +0530

    refactor(scripts/gcpsetup): remove unnecessay stuff

--- scripts/gcpsetup.sh
+++ scripts/gcpsetup.sh
@@ -4,22 +4,15 @@
 sudo mount -o remount,rw,noatime,commit=600 /
 
 packages=(
-  "7zip"
-  "android-sdk-platform-tools" "aria2" "arj" "axel"
-  "bc" "bison" "brotli" "btop" "build-essential"
-  "cabextract" "ccache" "cpio" "curl"
-  "detox" "device-tree-compiler"
-  "flex"
-  "g++-multilib" "gawk" "gcc-multilib" "git" "git-lfs" "gnupg" "gperf"
-  "imagemagick"
-  "lib32ncurses-dev" "lib32readline-dev" "lib32z1-dev" "liblz4-dev" "liblz4-tool" "liblzma-dev" "libncurses6" "libncurses-dev" "libdw-dev" "libelf-dev" "libsdl1.2-dev" "libssl-dev" "libxml2" "libxml2-utils" "linux-modules-extra-$(uname -r)" "lz4" "lzop"
-  "micro" "mpack"
-  "p7zip-full" "p7zip-rar" "pngcrush" "protobuf-compiler" "python3-pip" "python3-protobuf" "python-is-python3"
-  "rar" "rename" "ripgrep" "repo" "rsync"
-  "schedtool" "sharutils" "squashfs-tools"
-  "unace" "unrar" "unzip" "uudeview"
-  "xsltproc"
-  "zip" "zlib1g-dev" "zram-tools" "zstd"
+  # lineage
+  "bc" "bison" "build-essential" "ccache" "curl" "flex" "g++-multilib" "gcc-multilib"
+  "git" "git-lfs" "gnupg" "gperf" "protobuf-compiler" "python3-protobuf" "python-is-python3"
+  "repo" "lib32readline-dev" "lib32z1-dev" "libdw-dev" "libelf-dev" "lib32ncurses-dev"
+  "libncurses6" "libncurses-dev" "lz4" "libsdl1.2-dev" "libssl-dev" "libxml2" "libxml2-utils"
+  "lzop" "pngcrush" "rsync" "schedtool" "squashfs-tools" "xsltproc" "zip" "zlib1g-dev"
+
+  # extra
+  "btop" "linux-modules-extra-$(uname -r)" "micro" "zram-tools" "zstd"
 )
 
 # Install all necessary packages
@@ -35,21 +28,11 @@ git config --global core.untrackedCache true
 git config --global gc.auto 0
 git lfs install
 
-# Install uv
-if [ ! -f "$HOME/.local/bin/uv" ]; then
-  curl -LSs https://astral.sh/uv/install.sh | bash
-fi
-
 # Setup RBE
 if [ ! -d "$HOME/.rbe" ]; then
   wget -q https://github.com/xyz-sundram/Releases/releases/download/client-linux-amd64/client-linux-amd64.zip
   unzip -q client-linux-amd64.zip -d ~/.rbe
   rm client-linux-amd64.zip
-fi
-
-# Download stock firmware
-if [ ! -f "$HOME/.firmware.zip" ]; then
-  curl -LSs https://mirrors.lolinet.com/firmware/lenomola/2024/eqe/official/RETAIL/EQE_RETAIL_15_V1UMS35H.10-67-7-2_subsidy-DEFAULT_regulatory-DEFAULT_cid50_CFC.xml.zip -o ~/.firmware.zip
 fi
 
 # ZRAM Setup
